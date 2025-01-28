f2e628bd1a0774b00338c61681d539eb94446fee
wilgnerjr <76569878+wilgnerjr@users.noreply.github.com>
2025-01-28 02:28:26 -0400

    [

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function redirecionar() {
   const codigo = document.getElementById("codigo").value.trim().toUpperCase(); // Captura o valor digitado e converte para maiúsculas
   const links = {
-    ADMIN: "https://web.whatsapp.com/", // Link para o código ADMIN
+    ADMIN: "restaurantes.html", // Link para o código ADMIN
     USER: "https://link-para-usuario.com", // Link para o código USER
     GUEST: "https://link-para-visitante.com", // Link para o código GUEST
     "77A": "https://www.instagram.com/wilgner_2003/", // Link para o código 77A
